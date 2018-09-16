aee366d5d5078941aacc5814a1bfc23354c84356
s06181110 <s06181110@gmail.com>
2018-09-16 16:27:42 +0900

    add user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,48 @@
 class UsersController < ApplicationController
+  #ユーザー登録画面
   def sign_up
+    @user = User.new
   end
+  #ユーザー登録処理
+  def create
+    @user = User.new
+    @user.name = params[:name]
+    @user.password = params[:password]
+    @user.email = params[:email]
+    if @user.save
+      redirect_to("/users/index")
+    else
+      render("users/sign_up")
+    end
+  end
+ 
+  #ユーザー一覧表示
+  def index
+    @users = User.all
+  end
+ 
+  #ユーザー情報詳細表示
+  def show
+  end
+ 
+  #ユーザー情報編集画面
+  def edit
+  end
+ 
+  #ユーザー情報更新処理
+  def update
+  end
+ 
+  #ユーザー情報削除処理
+  def destroy
+  end
+ 
+  #ユーザーログイン画面
+  def login
+  end
+ 
+  #ユーザーログアウト処理
+  def logout
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,16 @@
 Rails.application.routes.draw do
-  get 'users/sign_up'
+  get 'posts/index'
+  get "users/sign_up" => "users#sign_up"#登録
+  post "users/create" => "users#create" #登録処理
+  get "users/index" => "users#index" #一覧表示
+  get "users/:id/show" => "users#show" #詳細表示
+  get "users/:id/edit" => "users#edit" #編集
+  post "users/:id/update" => "users#update" #更新処理
+  post "users/:id/destroy" => "users#destroy" #削除処理
+  get "login" => "users#login" #ログイン
+  post "users/:id/logout" => "users#logout" #ログアウト
+
+  
   get "/" => "home#top"
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
