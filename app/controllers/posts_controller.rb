05b148101d7cab53f58a580631a9347c6d809363
s06181110 <s06181110@gmail.com>
2018-09-17 06:03:03 +0900

    update post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,27 +7,34 @@ class PostsController < ApplicationController
   def new 
     @post = Post.new
   end
-=begin 
+
   def create
     @post = Post.new
-    @post.member = params[:member]
-    if params[:yori]
-      @post.yori = true
-    end
-    if params[:tyu]
-      @post.tyu = true
-    end
-    if params[:hiki]
-      @post.hiki = true
-    end
-    if @post.member
+    @post.content = ""
+    members = []
+    members = params[:members]
+    if(params[:members][0] != "noselect" && (params[:yori] || params[:tyu] || params[:hiki]))
+      members.each do |member|
+        @post.content.concat(member)
+        if params[:yori]
+          @post.content.concat(" ヨリ")
+        end
+        if params[:tyu]
+          @post.content.concat(" チュウ")
+        end
+        if params[:hiki]
+          @post.content.concat(" ヒキ")
+        end
+        @post.content.concat(" \n")
+      end
+      if params[:comment]
+        @post.comment = params[:comment]
+      end
       @post.save
       redirect_to("/posts/index")
     else
       render("posts/new")
     end
-
   end
 
- =end
 end
